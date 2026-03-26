4b0b10b044058e12d3f9f84baf10ba18005fbb8a
mrlnem <157147062+mrlnem@users.noreply.github.com>
2026-03-26 15:35:03 +0100

    Update model_cnn.py

---
+++ src/model_cnn.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+        self.fc = nn.Linear(64, 3)
+
+    def forward(self, x):
+        x = self.net(x)
+        x = x.view(x.size(0), -1)
+        return self.fc(x)
